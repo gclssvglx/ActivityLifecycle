ad1953c03c510ac3003d33ffa8facc33fbdb0a6a
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-01-17 14:57:58 +0000

    Add basic state machine output and diagram

--- app/src/main/java/com/gclewis/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/gclewis/activitylifecycle/MainActivity.kt
@@ -3,44 +3,86 @@ package com.gclewis.activitylifecycle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.gclewis.activitylifecycle.ui.theme.ActivityLifecycleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("Activity Launched...")
+        println("onCreate")
         setContent {
             ActivityLifecycleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ShowImage()
                 }
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Activity Running...")
+        println("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("App process killed...")
+        println("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Activity Shutdown...")
+        println("onDestroy")
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShowImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.activity_lifecycle)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ActivityLifecyclePreview() {
     ActivityLifecycleTheme {
-        Greeting("Android")
+        ShowImage()
     }
 }
